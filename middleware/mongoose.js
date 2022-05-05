87f743cd066a35743ee8d2c714382095c10cf670
PranxXxD <71432749+PranxXxD@users.noreply.github.com>
2022-05-05 23:39:07 +0530

    Update mongoose.js

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // Initiating the mongoose connection
 
 const connectDb = (handler) => async (req, res) => {
-  console.log(process.env.MONGO_URI)
+  // console.log(process.env.MONGO_URI)
   if (mongoose.connections[0].readyState) {
     return handler(req, res);
   }
