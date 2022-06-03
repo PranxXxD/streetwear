51f4a70b7163ef7b307c580db506c780fc37acc2
PranxXxD <71432749+PranxXxD@users.noreply.github.com>
2022-06-03 23:59:31 +0530

    added the out of stock functionality in shirts

--- pages/shirts.js
+++ pages/shirts.js
@@ -3,6 +3,8 @@ import Link from "next/link";
 import Product from "../models/Product";
 import mongoose from "mongoose";
 import { FaRupeeSign } from "react-icons/fa";
+import Image from "next/image";
+import outofstock from "../images/outofstock.png";
 
 const shirts = ({ products }) => {
   // console.log(products);
@@ -14,7 +16,34 @@ const shirts = ({ products }) => {
             {/* rendering the products using map method */}
             {/* products is an object so need to use OBject.keys method to display  */}
             {Object.keys(products).length === 0 && (
-              <p>Sorry, shirts are out of stock, new stock coming soon!</p>
+              <>
+                <section class="text-gray-600 body-font">
+                  <div class="container mx-auto flex md:flex-row flex-col items-center">
+                    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-1/2 md:mb-0">
+                      <Image
+                        className="object-cover object-center rounded"
+                        width={400}
+                        height={200}
+                        src={outofstock}
+                      />
+                    </div>
+                    <div class="lg:flex-grow md:w-1/2 lg:pl-10 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                      <h2 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                        Sorry, hoodies are out of stock,
+                        <br class="hidden lg:inline-block" />
+                        new stock coming soon!
+                      </h2>
+                      <p></p>
+
+                      <div class="flex justify-center">
+                        <button class="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
+                          Get Notify
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </>
             )}
             {Object.keys(products).map((item) => {
               return (
