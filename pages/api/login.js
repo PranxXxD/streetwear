86b7c4c828ce73bbf11e496313c44444f9a689e5
PranxXxD <71432749+PranxXxD@users.noreply.github.com>
2022-06-06 14:31:57 +0530

    signup & login
    Added the encryption and decryption functionality to the login and signup API's

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,26 +2,37 @@
 
 import connectDb from "../../middleware/mongoose";
 import User from "../../models/User";
+var CryptoJS = require("crypto-js");
 
 const handler = async (req, res) => {
   if (req.method == "POST") {
     // console.log(req.body);
     // find the user by the email id
     let user = await User.findOne({ email: req.body.email });
+    // decrypting the password
+    var bytes = CryptoJS.AES.decrypt(user.password, "secret123");
+    // console.log(bytes.toString(CryptoJS.enc.Utf8));
+    var decryptedPass = bytes.toString(CryptoJS.enc.Utf8);
+
     if (user) {
       // check the given inputs from the user and then authenticate
-      if (req.body.email == user.email && req.body.password == user.password) {
+      if (req.body.email == user.email && req.body.password == decryptedPass) {
         res
           .status(200)
           .json({ success: true, email: user.email, name: user.name });
       } else {
         //   throw the error message if the user enter the wrong credentials
-        res.status(400).json({ success: false, error: "Invalid Credentials" });
+        res.status(200).json({ success: false, error: "Invalid Credentials" });
       }
+    } else {
+      //   throw the error message if the user enter the wrong credentials
+      res.status(200).json({ success: false, error: "No user found" });
     }
   } else {
     //   throw error if user not found in database
-    res.status(400).json({ success: false, error: "No user Found" });
+    res
+      .status(400)
+      .json({ success: false, error: "This Method is not allowed" });
   }
 };
 
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,11 +2,19 @@
 
 import connectDb from "../../middleware/mongoose";
 import User from "../../models/User";
+var CryptoJS = require("crypto-js");
 
 const handler = async (req, res) => {
   if (req.method == "POST") {
-    // console.log(req.body);
-    let u = new User(req.body);
+    console.log(req.body);
+    // destructuring object
+    const { name, email } = req.body;
+    // encrypting the password
+    let u = new User({
+      name,
+      email,
+      password: CryptoJS.AES.encrypt(req.body.password, "secret123").toString(),
+    });
     await u.save();
     res.status(200).json({ success: "Success" });
   } else {
