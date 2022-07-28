65ca0ea9c447d619776b916e1b3527c3258c4933
PranxXxD <71432749+PranxXxD@users.noreply.github.com>
2022-07-28 17:12:17 +0530

    admin/imageuploader
    fetching and displaying images from data base

--- pages/admin/imageuploader.js
+++ pages/admin/imageuploader.js
@@ -3,100 +3,26 @@ import FullLayout from "../../src/layouts/FullLayout";
 import theme from "../../src/theme/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import Head from "next/head";
-import { Grid, ImageList, ImageListItem } from "@mui/material";
+import { Grid, TextField, ImageList, ImageListItem } from "@mui/material";
 import BaseCard from "../../src/components/baseCard/BaseCard";
-
-function srcset(image, size, rows = 1, cols = 1) {
-  return {
-    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${size * cols}&h=${
-      size * rows
-    }&fit=crop&auto=format&dpr=2 2x`,
-  };
-}
-
-const itemData = [
-  {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "Breakfast",
-    rows: 2,
-    cols: 1,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "Burger",
-    rows: 2,
-    cols: 1,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "Camera",
-    rows: 2,
-    cols: 1,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
-    rows: 2,
-    cols: 1,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-    rows: 2,
-    cols: 1,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-    author: "@arwinneil",
-    rows: 2,
-    cols: 1,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
-    rows: 2,
-    cols: 1,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-    rows: 2,
-    cols: 1,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-    rows: 2,
-    cols: 1,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-    rows: 2,
-    cols: 1,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-    rows: 2,
-    cols: 1,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-    rows: 2,
-    cols: 1,
-  },
-];
+import Product from "../../models/Product";
+import mongoose from "mongoose";
+import TransformImage from "../../components/images";
 
 // adding an image to database and showing in dashboard -> pending
-const ImageUploader = () => {
+const ImageUploader = ({ products }) => {
+  function srcset(size, rows = 1, cols = 2) {
+    return {
+      src: `${products}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+      srcSet: `${products}?w=${size * cols}&h=${
+        size * rows
+      }&fit=crop&auto=format&dpr=2 2x`,
+    };
+  }
+  // create the widget
   const [imagePublicId, setImagePublicId] = useState("");
 
   const openWidget = () => {
-    // create the widget
     const widget = window.cloudinary.createUploadWidget(
       {
         cloudName: "pranxxxd",
@@ -143,24 +69,26 @@ const ImageUploader = () => {
                   upload
                 </button>
                 <ImageList
-                  sx={{ height: 450 }}
+                  sx={{ height: 400 }}
                   variant="quilted"
                   cols={4}
-                  rowHeight={121}
+                  rowHeight={500}
                 >
-                  {itemData.map((item) => (
-                    <ImageListItem
-                      key={item.img}
-                      cols={item.cols || 1}
-                      rows={item.rows || 1}
-                    >
+                  {products.map((product) => {
+                    return (
+                      // <ImageListItem
+                      //   key={product.img}
+                      //   cols={product.cols || 1}
+                      //   rows={product.rows || 1}
+                      // >
                       <img
-                        {...srcset(item.img, 121, item.rows, item.cols)}
-                        alt={item.title}
-                        loading="lazy"
+                        // {...srcset(product.rows, product.cols)}
+                        alt="ecommerce"
+                        className="h-[30vh] md:h-[36vh] block m-auto p-4 row-auto col-auto"
+                        src={product.img}
                       />
-                    </ImageListItem>
-                  ))}
+                    );
+                  })}
                 </ImageList>
               </BaseCard>
             </Grid>
@@ -170,5 +98,18 @@ const ImageUploader = () => {
     </>
   );
 };
+
+export async function getServerSideProps(context) {
+  let error = null;
+
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+  let products = await Product.find();
+  // console.log(products);
+  return {
+    props: { products: JSON.parse(JSON.stringify(products)) }, // will be passed to the page component as props
+  };
+}
 
 export default ImageUploader;
